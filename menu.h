4f7775138c476196d7a3c72fee824662813ef75f
G1sTTon <jabbari.gaston2004@gmail.com>
2025-04-25 03:06:35 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -7,6 +7,7 @@
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
+#define MAX_OBSTACLES 6 // Max obstacles for hard level
 
 typedef struct {
     SDL_Surface *background;
@@ -15,11 +16,26 @@ typedef struct {
 } Background;
 
 typedef struct {
+    SDL_Surface *image; // claquette.jpeg or nike.jpg
+    SDL_Rect position;
+    int velocity_x; // Speed in pixels/frame (negative for leftward movement)
+    int active; // 1 if active, 0 if available for respawn
+    int type; // 0 for claquette, 1 for nike
+} Obstacle;
+
+typedef struct {
     SDL_Surface *button_inactive;
     SDL_Surface *button_active;
     SDL_Rect position;
     int is_hovered;
 } Button;
+
+typedef struct {
+    Background bg;
+    SDL_Rect bounding_box; // AABB for collision (e.g., at center of camera)
+    int lives; // Number of lives
+    int score; // Player score
+} Player;
 
 void load_backgrounds(Background backgrounds[]);
 void load_buttons(Button buttons[]);
@@ -32,10 +48,16 @@ char* get_user_input(SDL_Surface *screen, TTF_Font *font, SDL_Color color, int m
 void render_time(SDL_Surface *screen, TTF_Font *font, SDL_Color color, Uint32 startTime, int player);
 int initBackground(Background *b, const char *path, int split_screen);
 int scrolling(Background *b, int dx, int dy);
-void renderBackground(SDL_Surface *screen, Background *b, TTF_Font *font, SDL_Color color, Uint32 startTime, int split_screen);
-void splitScreen(SDL_Surface *screen, Background *player1Bg, Background *player2Bg, TTF_Font *font, SDL_Color color, Uint32 startTime);
+void renderBackground(SDL_Surface *screen, Player *p, Obstacle obstacles[], int num_obstacles, TTF_Font *font, SDL_Color color, Uint32 startTime, int split_screen, int player_num);
+void splitScreen(SDL_Surface *screen, Player *player1, Player *player2, Obstacle obstacles[], int num_obstacles, TTF_Font *font, SDL_Color color, Uint32 startTime);
 void display_guide(SDL_Surface *screen, TTF_Font *font);
 void prompt_guide(SDL_Surface *screen, TTF_Font *font, SDL_Color color, char *response);
 void display_successive_backgrounds(SDL_Surface *screen);
+void initObstacles(Obstacle obstacles[], int max_obstacles);
+void updateObstacles(Obstacle obstacles[], int num_obstacles, int level, int screen_width, int screen_height);
+void renderObstacles(SDL_Surface *screen, Obstacle obstacles[], int num_obstacles, SDL_Rect *camera_pos);
+int checkCollision(SDL_Rect a, SDL_Rect b);
+void initPlayer(Player *p, const char *bg_path, int split_screen, int lives, int score, int player_num);
+void renderPlayerStats(SDL_Surface *screen, Player *p, TTF_Font *font, SDL_Color color, int player_num);
 
 #endif
